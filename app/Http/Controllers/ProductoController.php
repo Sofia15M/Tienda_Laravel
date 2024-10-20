5ec15d89cb319a92076977876f80074ff172ecec
Sofia15M <sofiaortizacevedo2007@gmail.com>
2024-10-20 15:31:14 -0500

    app.php se añade forma correcta el except csrf productos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -14,29 +14,30 @@ class ProductoController extends Controller
         return response()->json($productos);
     }
 
+    public function show(string $id)
+    {
+        $producto = Producto::where('id', $id)->firstOrFail(); // Cambia esto si es necesario
+        return response()->json($producto, 200);
+    }
+
     public function store(ProductoRequest $request)
     {
         $producto = Producto::create($request->validated());
         return response()->json($producto, 201);
     }
 
-        public function show(string $id)
-    {
-        $producto = Producto::where('id', $id)->firstOrFail(); // Cambia esto si es necesario
-        return response()->json($producto, 200);
-    }
-
     public function update(ProductoRequest $request, string $id)
     {
         $producto = Producto::findOrFail($id);
         $producto->update($request->validated());
-        return response()->json($producto);
+
+        return response()->json($producto, 200);
     }
 
     public function destroy(string $id)
     {
         $producto = Producto::findOrFail($id);
         $producto->delete();
-        return response()->json(null, 204);
+        return response()->json(['message' => 'Producto eliminado con éxito.'], 200);
     }
 }
